be20375f641ad344ff94d8e8c70eee385022608d
Xerath <yoondj98@naver.com>
2025-03-28 22:26:06 +0900

    [Init] 프로젝트 생성

---
+++ OneThing/App/OneThingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneThingApp.swift
+//  OneThing
+//
+//  Created by 윤동주 on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct OneThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ OneThingTests/OneThingTests.swift
@@ -0,0 +1,17 @@
+//
+//  OneThingTests.swift
+//  OneThingTests
+//
+//  Created by 윤동주 on 3/15/25.
+//
+
+import Testing
+@testable import OneThing
+
+struct OneThingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
