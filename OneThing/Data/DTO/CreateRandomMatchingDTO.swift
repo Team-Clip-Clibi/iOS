b7a6f895c46f27bffa1cf8a57cce1175e77e031f
Xerath <yoondj98@naver.com>
2025-05-07 09:03:52 +0900

    [Chore] 필요한 DTO 추가

---
+++ OneThing/Data/DTO/CreateRandomMatchingDTO.swift
@@ -0,0 +1,15 @@
+//
+//  CreateRandomMatchingDTO.swift
+//  OneThing
+//
+//  Created by 윤동주 on 3/23/25.
+//
+
+import Foundation
+
+struct CreateRandomMatchingDTO: Codable {
+    var city: String
+    var location: String
+    var restaurantName: String
+    var meetingTime: String
+}
