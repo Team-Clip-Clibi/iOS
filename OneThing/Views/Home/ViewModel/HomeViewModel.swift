f6c42dc72ede18c448d658b91a786d975bc90c08
hyeonsik971029 <hyeonsik971029@gmail.com>
2025-05-10 15:14:12 +0900

    [추가] 홈 화면 뷰모델 추가

--- OneThing/Views/Home/ViewModel/HomeViewModel.swift
+++ OneThing/Views/Home/ViewModel/HomeViewModel.swift
@@ -10,4 +10,56 @@ import Foundation
 @Observable
 class HomeViewModel {
     
+    struct State: Equatable {
+        fileprivate(set) var isUnReadNotificationEmpty: Bool
+        fileprivate(set) var noticeInfos: [NoticeInfo]
+        fileprivate(set) var matchingSummaryInfos: [MatchingSummaryInfo]
+    }
+    
+    var currentState: State
+    
+    private let unReadNotificationUseCase: GetUnReadNotificationUseCase
+    private let noticeUseCase: GetNoticeUseCase
+    private let matchingSummaryUseCase: GetMatchingSummaryUseCase
+    
+    init(
+        unReadNotificationUseCase: GetUnReadNotificationUseCase = GetUnReadNotificationUseCase(),
+        noticeUseCase: GetNoticeUseCase = GetNoticeUseCase(),
+        matchingSummaryUseCase: GetMatchingSummaryUseCase = GetMatchingSummaryUseCase()
+    ) {
+        
+        self.currentState = State(
+            isUnReadNotificationEmpty: true,
+            noticeInfos: [],
+            matchingSummaryInfos: []
+        )
+        
+        self.unReadNotificationUseCase = unReadNotificationUseCase
+        self.noticeUseCase = noticeUseCase
+        self.matchingSummaryUseCase = matchingSummaryUseCase
+    }
+    
+    func isUnReadNotificationEmpty() async {
+        do {
+            self.currentState.isUnReadNotificationEmpty = try await self.unReadNotificationUseCase.isUnReadNotificationEmpty()
+        } catch {
+            self.currentState.isUnReadNotificationEmpty = true
+        }
+    }
+    
+    func notice() async {
+        do {
+            self.currentState.noticeInfos = try await self.noticeUseCase.execute()
+        } catch {
+            self.currentState.noticeInfos = []
+        }
+    }
+    
+    func matchingSummary() async {
+        do {
+            self.currentState.matchingSummaryInfos = try await self.matchingSummaryUseCase.execute()
+        } catch {
+            self.currentState.matchingSummaryInfos = []
+        }
+    }
 }
