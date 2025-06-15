515894b58d558a8ac2fb4dd01d7ec50f5df40e8b
hyeonsik971029 <hyeonsik971029@gmail.com>
2025-06-15 17:25:15 +0900

    [추가] 모임 후기 화면에 사용될 Components 추가
     - AttendeesCheckView: 참석자 확인 용 뷰
     - MeetingReviewGuideMessageView: 상단 가이드 메시지 뷰
     - NegativePointsView: 아쉬운 점 선택 뷰
     - PositivePointsView: 좋앗던 점 선택 뷰
     - ReviewContentView: 자세한 경험을 적는 뷰

---
+++ OneThing/Views/Home/Meeting/MeetingReview/Components/ReviewContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ReviewContentView.swift
+//  OneThing
+//
+//  Created by 오현식 on 6/10/25.
+//
+
+import SwiftUI
+
+struct ReviewContentView: View {
+    
+    enum Constants {
+        enum Text {
+            static let reviewContentTitle: String = "자세한 경험을 알려주세요"
+            static let reviewPlaceholder: String = "소중한 의견을 남겨주세요 (선택사항)"
+        }
+    }
+    
+    @Binding var reviewContent: String
+    
+    var body: some View {
+        
+        VStack {
+            
+            Text(Constants.Text.reviewContentTitle)
+                .otFont(.subtitle1)
+                .foregroundStyle(.gray800)
+                .padding(.horizontal, 16)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer().frame(height: 16)
+            
+            ZStack {
+                
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(.gray300, lineWidth: 1)
+                
+                TextEditor(text: $reviewContent)
+                    .otFont(.body2)
+                    .foregroundStyle(.gray800)
+                    .tint(.purple400)
+                    .multilineTextAlignment(.leading)
+                    .contentMargins(.all, 16)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .overlay(
+                        Text(Constants.Text.reviewPlaceholder)
+                            .otFont(.body2)
+                            .foregroundStyle(self.reviewContent.isEmpty ? .gray500: .clear)
+                            .padding(24)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                    )
+            }
+            .padding(.horizontal, 16)
+            .frame(maxWidth: .infinity)
+            .frame(height: 152)
+        }
+    }
+}
+
+#Preview {
+    ReviewContentView(reviewContent: .constant(""))
+}
