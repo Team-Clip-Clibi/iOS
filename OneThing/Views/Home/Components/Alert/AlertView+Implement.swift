b3e63cf7cd912d23df79d65e835ca2e683299e5f
hyeonsik971029 <hyeonsik971029@gmail.com>
2025-06-17 17:39:33 +0900

    [추가] Alert 관리 방식 변경
     - 기존 ZStack { ... if isPresented { AlertView() } }에서 modifier 사용
     - 준비중 팝업 미리 구현해서 사용

---
+++ OneThing/Views/Home/Components/Alert/AlertView+Implement.swift
@@ -0,0 +1,35 @@
+//
+//  AlertView+Static.swift
+//  OneThing
+//
+//  Created by 오현식 on 6/17/25.
+//
+
+import SwiftUI
+
+extension View {
+    
+    func showPreparing(isPresented: Binding<Bool>) -> some View {
+        
+        let title: String = "서비스 준비 중이에요"
+        let message: String = "빠른 시일내에 준비하여 찾아뵐게요"
+        let confirmButtonTitle: String = "확인"
+        
+        let action = AlertAction(
+            title: confirmButtonTitle,
+            style: .confirm,
+            action: { isPresented.wrappedValue = false }
+        )
+        
+        return self.modifier(
+            AlertModifier(
+                isPresented: isPresented,
+                imageResource: .comingsoon,
+                title: title,
+                message: message,
+                actions: [action],
+                dismissWhenBackgroundTapped: true
+            )
+        )
+    }
+}
