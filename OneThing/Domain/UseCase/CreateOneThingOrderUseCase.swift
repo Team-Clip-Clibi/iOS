853a45c7ae6775a07ce8fe3a78ad71c49353478c
Xerath <yoondj98@naver.com>
2025-06-29 14:17:09 +0900

    [Fix] 원띵 토스 결제 API DTO 변경 적용

---
+++ OneThing/Domain/UseCase/CreateOneThingOrderUseCase.swift
@@ -0,0 +1,35 @@
+//
+//  CreateOneThingOrderUseCase.swift
+//  OneThing
+//
+//  Created by 윤동주 on 6/17/25.
+//
+
+import Foundation
+
+struct CreateOneThingOrderUseCase {
+    private let repository: TossPaymentsRepository
+
+    init(repository: TossPaymentsRepository = TossPaymentsRepository()) {
+        self.repository = repository
+    }
+    
+    func execute(
+        topic: String,
+        district: District,
+        preferredDates: [PreferredDate],
+        tmiContent: String,
+        oneThingBudgetRange: BudgetRange,
+        oneThingCategory: OneThingCategory
+    ) async throws -> OneThingOrderResponse {
+        let request = OneThingOrderRequest(
+            topic: topic,
+            district: district,
+            preferredDates: preferredDates,
+            tmiContent: tmiContent,
+            oneThingBudgetRange: oneThingBudgetRange,
+            oneThingCategory: oneThingCategory
+        )
+        return try await repository.onethingsOrder(with: request)
+    }
+}
