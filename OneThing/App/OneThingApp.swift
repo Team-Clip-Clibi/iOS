3bbed51a694a334f9fe114ee2d9828dd24c87d80
hyeonsik971029 <hyeonsik971029@gmail.com>
2025-07-28 22:17:36 +0900

    [변경] AppDelegate 관련 파일로 분리

--- OneThing/App/OneThingApp.swift
+++ OneThing/App/OneThingApp.swift
@@ -6,72 +6,6 @@
 //
 
 import SwiftUI
-import FirebaseCore
-import FirebaseMessaging
-import UserNotifications
-
-class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        // Firebase 초기화
-        FirebaseApp.configure()
-        
-        // UNUserNotificationCenter delegate 설정
-        UNUserNotificationCenter.current().delegate = self
-        
-        // Firebase Messaging delegate 설정
-        Messaging.messaging().delegate = self
-        
-        registerForPushNotifications()
-        
-        return true
-    }
-    
-    func registerForPushNotifications() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
-            if let error = error {
-                print("푸시 권한 요청 에러: \(error.localizedDescription)")
-                return
-            }
-            
-            if granted {
-                DispatchQueue.main.async {
-                    UIApplication.shared.registerForRemoteNotifications()
-                }
-            } else {
-                print("푸시 권한 거부됨")
-            }
-        }
-    }
-    
-    // APNS 등록 성공 시 호출: deviceToken을 Firebase Messaging에 전달
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        // APNS 토큰 설정
-        Messaging.messaging().apnsToken = deviceToken
-        
-        // 디버그용 APNS 토큰 출력
-        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
-        let tokenString = tokenParts.joined()
-        print("APNS 토큰: \(tokenString)")
-    }
-    
-    // APNS 등록 실패 시 호출
-    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        print("원격 알림 등록 실패: \(error.localizedDescription)")
-    }
-}
-
-// MARK: - MessagingDelegate
-extension AppDelegate: MessagingDelegate {
-    // APNS 토큰이 설정된 후 Firebase가 FCM 토큰을 갱신하면 호출됩니다.
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        guard let token = fcmToken else { return }
-        
-        print("Firebase FCM 토큰: \(token)")
-        
-        UserDefaults.standard.set(token, forKey: "firebaseToken")
-    }
-}
 
 @main
 struct OneThingApp: App {
@@ -140,8 +74,7 @@ struct LaunchScreenWrapper: UIViewControllerRepresentable {
         return storyboard.instantiateInitialViewController() ?? UIViewController()
     }
     
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-    }
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) { }
 }
 
 extension String {
