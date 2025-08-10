00ac84c809bff1ff6ebc7128c1a36c46bce48125
hyeonsik971029 <hyeonsik971029@gmail.com>
2025-08-10 18:56:46 +0900

    [변경] Main 관련 변경

--- OneThing/App/OneThingApp.swift
+++ OneThing/App/OneThingApp.swift
@@ -7,74 +7,28 @@
 
 import SwiftUI
 
+import KakaoSDKAuth
+
 @main
 struct OneThingApp: App {
+    
     // Firebase 설정을 위한 AppDelegate 등록
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
-    @State var appStateManager = AppStateManager()
-    @State var isReady = false
-    
-    private let sessionStore: SessionStoring = UserDefaultsSessionStore()
-    private let authRepository = AuthRepository()
+    @State private var appCoordinator: AppCoordinator = AppCoordinator(dependencies: AppDIContainer())
     
     var body: some Scene {
         WindowGroup {
-            if isReady {
-                MainView()
-                    .toolbar(.hidden, for: .navigationBar)
-                    .environment(appStateManager)
-            } else {
-                LaunchScreenWrapper()
-                    .ignoresSafeArea(.all)
-                    .onAppear {
-                        Task {
-                            try await performInitialSetup()
-                            // 초기 설정 작업 완료 후 isReady 업데이트
-                            isReady = true
-                        }
+            Group {
+                
+                self.appCoordinator.start()
+                    .onOpenURL { url in
+                        if AuthApi.isKakaoTalkLoginUrl(url) { _ = AuthController.handleOpenUrl(url: url)}
                     }
             }
+            .environment(\.appCoordinator, self.appCoordinator)
         }
     }
-    
-    private func performInitialSetup() async throws {
-        let socialId = sessionStore.socialId ?? ""
-        let platform = sessionStore.platform ?? ""
-        let firebaseToken = sessionStore.firebaseToken ?? ""
-        let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "0.0"
-        
-        let signInDTO = SignInDTO(
-//            socialId: "".randomString(length: 10),
-            socialId: socialId,
-            platform: platform,
-            osVersion: version,
-            firebaseToken: firebaseToken,
-            isAllowNotify: true
-        )
-        
-        do {
-            let response = try await authRepository.usersSignIn(with: signInDTO)
-            TokenManager.shared.accessToken = response.accessToken
-            TokenManager.shared.refreshToken = response.refreshToken
-            
-            self.appStateManager.isSignedIn = true
-        } catch {
-            self.appStateManager.isSignedIn = false
-        }
-        
-        try await NotificationManager.shared.requestAuthorization()
-        HapticManager.shared.setEnable(true)
-    }
-}
-
-struct LaunchScreenWrapper: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> UIViewController {
-        let storyboard = UIStoryboard(name: "Launch Screen", bundle: nil)
-        return storyboard.instantiateInitialViewController() ?? UIViewController()
-    }
-    
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) { }
 }
 
 extension String {
