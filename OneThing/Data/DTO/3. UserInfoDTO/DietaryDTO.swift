80e9ab8927d95c9c71c73c65dfa7e0589dbbbae4
hyeonsik971029 <hyeonsik971029@gmail.com>
2025-08-12 18:40:53 +0900

    [수정] "다 잘먹어요" 선택지를 최상단으로 수정

--- OneThing/Data/DTO/3. UserInfoDTO/DietaryDTO.swift
+++ OneThing/Data/DTO/3. UserInfoDTO/DietaryDTO.swift
@@ -12,20 +12,20 @@ struct DietaryDTO: Codable {
 }
 
 enum DietaryType: String, CaseIterable, Identifiable {
+    case all        = "ALL"
     case vegan      = "VEGAN"
     case vegetarian = "VEGETARIAN"
     case glutenFree = "GLUTEN_FREE"
-    case all        = "ALL"
     case etc        = "ETC"
     
     var id: String { self.rawValue }
     
     var toKorean: String {
         switch self {
+        case .all:          return "다 잘먹어요"
         case .vegan:        return "비건이에요"
         case .vegetarian:   return "베지테리언이에요"
         case .glutenFree:   return "글루텐프리를 지켜요"
-        case .all:          return "다 잘먹어요"
         case .etc:          return "기타"
         }
     }
