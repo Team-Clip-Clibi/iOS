9c80f82b3a2f79d8f84c79a732d234d667ded647
hyeonsik971029 <hyeonsik971029@gmail.com>
2025-08-12 17:52:58 +0900

    [수정] 매칭 관련 메인 뷰 이미지 및 텍스트 수정

--- OneThing/Views/Main/Home/Matching/OneThing/OnethingMatchingMainView.swift
+++ OneThing/Views/Main/Home/Matching/OneThing/OnethingMatchingMainView.swift
@@ -9,16 +9,18 @@ import SwiftUI
 
 struct OnethingMatchingMainView: View {
     
-    enum ConstText {
+    enum Constants {
         
-        static let naviTitle = "원띵 모임 신청"
-        
-        static let title = "매주 토, 일\n원띵 모임에서 만나요"
-        static let subTitle = "대화 주제가 맞는 사람과 만나 ‘Onething’을 나눠요"
-        
-        static let message = "관심있는 하나의 주제로,\n집중적으로 대화해요"
-        
-        static let nextButtonTitle = "다음"
+        enum Text {
+            static let naviTitle = "원띵 모임 신청"
+            
+            static let title = "매주 토, 일\n원띵 모임에서 만나요"
+            static let subTitle = "대화 주제가 맞는 사람과 만나 ‘Onething’을 나눠요"
+            
+            static let message = "관심있는 하나의 주제로,\n집중적으로 대화해요"
+            
+            static let nextButtonTitle = "다음"
+        }
     }
     
     @Environment(\.homeCoordinator) var homeCoordinator
@@ -31,13 +33,14 @@ struct OnethingMatchingMainView: View {
             
             VStack {
                 
-                Spacer().frame(height: 16)
-                
                 ScrollView(.vertical, showsIndicators: false) {
+                    
+                    Spacer().frame(height: 16)
+                    
                     GuideMessageView(
                         isChangeSubTitleColor: .constant(false),
-                        title: ConstText.title,
-                        subTitle: ConstText.subTitle
+                        title: Constants.Text.title,
+                        subTitle: Constants.Text.subTitle
                     )
                     
                     ZStack {
@@ -45,9 +48,18 @@ struct OnethingMatchingMainView: View {
                         RoundedRectangle(cornerRadius: 24)
                             .fill(.purple100)
                         
-                        Image(.onethingMatchingMain)
+                        VStack(spacing: 32) {
+                            Image(.onethingMatchingMain)
+                            
+                            Text(Constants.Text.message)
+                                .otFont(.title1)
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(.gray800)
+                        }
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        
                     }
-                    .padding(.horizontal, 16)
+                    .padding([.top, .horizontal], 16)
                     .frame(maxWidth: .infinity)
                     .frame(height: 460)
                 }
@@ -56,13 +68,13 @@ struct OnethingMatchingMainView: View {
                 
                 BottomButton(
                     isClickable: .constant(true),
-                    title: ConstText.nextButtonTitle,
+                    title: Constants.Text.nextButtonTitle,
                     buttonTapAction: { self.homeCoordinator.push(to: .home(.onething(.category))) }
                 )
             }
         }
         .navigationBar(
-            title: ConstText.naviTitle,
+            title: Constants.Text.naviTitle,
             hidesBottomSeparator: true,
             onBackButtonTap: { self.homeCoordinator.pop() }
         )
